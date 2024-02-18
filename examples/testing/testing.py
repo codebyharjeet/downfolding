ba3fea3d6844c8a8e9b8d22a94cf423a14518aeb
Harjeet Singh <114513302+codebyharjeet@users.noreply.github.com>
2024-02-18 18:09:39 -0500

    temp disabling git actions

--- examples/testing/testing.py
+++ examples/testing/testing.py
@@ -74,6 +74,72 @@ ft1t2_mat_1, ft1t2_mat_2 = ducc.compute_ft1t2(f_blocks,t_blocks,n_a,n_b,n_orb)
 ft2t2_mat_1, ft2t2_mat_2, ft2t2_mat_3, ft2t2 = ducc.compute_ft2t2(f_blocks,t_blocks,n_a,n_b,n_orb,compute_three_body=False)
 """
 
-ducc_energy = ducc.compute_ducc_of(fmat, vmat, t1_amps, t2_amps, n_a, n_b, n_orb,n_occ, act_max, compute_three_body=False)
+#ducc_energy = ducc.compute_ducc_of(fmat, vmat, t1_amps, t2_amps, n_a, n_b, n_orb,n_occ, act_max, compute_three_body=False)
 
-print("Energy from exact diagonalisation using OpenFermion = ",ducc_energy)
+#print("Energy from exact diagonalisation using OpenFermion = ",ducc_energy)
+
+def del(i, j):
+  if i == j:
+    return 1
+  else:
+    return 0
+
+
+umat_1 = ducc.get_u(vmat,range(n_a),range(n_b))
+fmat -= umat_1
+
+fmat_pv = np.zeros((2*n_orb,2*n_orb))
+vmat_pv = np.zeros((2*n_orb,2*n_orb,2*n_orb,2*n_orb))
+
+# one electron integral
+for A in range(2*n_occ,2*act_max):
+    for B in range(2*n_occ,2*act_max):
+        fmat_pv[A,B] = fmat[A,B]
+
+for I in range(0,2*n_occ):
+    for J in range(0,2*n_occ):
+        fmat_pv[J,I] = fmat[J,I] 
+        fmat_pv[J,I] -= del(I,J)
+
+for I in range(0,2*n_occ):
+    for A in range(2*n_occ,2*act_max):
+        fmat_pv[I,A] = fmat[I,A]
+
+for A in range(2*n_occ,2*act_max):
+    for I in range(0,2*n_occ):
+        fmat_pv[A,I] = fmat[A,I]
+
+for B in range(2*n_occ,2*act_max):
+    for C in range(2*n_occ,2*act_max):
+        for A in range(2*n_occ,2*act_max):
+            for I in range(0,2*n_occ):
+                vmat_pv[B,C,I,A] = - vmat[B,C,A,I] 
+
+
+for K in range(0,2*n_occ):
+    for A in range(2*n_occ,2*act_max):
+        for J in range(0,2*n_occ):
+            for I in range(0,2*n_occ):
+                vmat_pv[A,K,J,I] = - vmat[K,A,J,I] 
+                fmat_pv[A,J] -= del(I,K)*fmat[A,J]
+
+fmat_spatial = ducc.get_spin_to_spatial(fmat,n_orb)
+
+vmat_spatial = ducc.get_spin_to_spatial(vmat,n_orb)
+
+h1 = fmat_spatial[0:act_max,0:act_max]
+h2 = vmat_spatial[0:act_max,0:act_max,0:act_max,0:act_max]  
+
+constant = 0
+cisolver = fci.direct_spin1.FCI()
+ecore = constant
+nroots = 1
+nelec = n_occ
+
+efci_orgnl,ci_orgnl = cisolver.kernel(h1, h2, h1.shape[1], nelec, ecore=ecore,nroots =nroots,verbose=100)
+fci_dim_orgnl = ci_orgnl.shape[0]*ci_orgnl.shape[1]
+print(" FCI:        %12.8f Dim:%6d"%(efci_orgnl,fci_dim_orgnl))
+
+print(" SCF:        %12.8f"%(E_scf))
+
+print("Correlation energy = ",efci_orgnl-E_scf)
