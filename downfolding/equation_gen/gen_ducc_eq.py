899d7bc090c2952064cf724cccb6296847db8010
Harjeet Singh <harjeetsinghgoru@gmail.com>
2026-05-07 18:52:59 -0400

    added wicked eq gen code

--- downfolding/equation_gen/gen_ducc_eq.py
+++ downfolding/equation_gen/gen_ducc_eq.py
@@ -35,11 +35,12 @@ W = w.utils.gen_op('v',2,'ovV','ovV')
 # [F,s1_ext]
 print("[F,s1_ext]")
 Fs1 = w.commutator(F,s1_ext)
-expr = wt.contract(Fs1,minrank=0,maxrank=6)
+expr = wt.contract(Fs1,minrank=0,maxrank=4)
 mbeq = expr.to_manybody_equations('fs1')
 for key in mbeq.keys():
 	for eq in mbeq[key]:
 		print(eq.compile('einsum'))
+
 # for eq in mbeq['|']:
 # 	print(eq.compile('einsum'))
 
@@ -55,7 +56,7 @@ for key in mbeq.keys():
 # [F,s2_ext]
 print("[F,s2_ext]")
 Fs2 = w.commutator(F,s2_ext)
-expr = wt.contract(Fs2,minrank=0,maxrank=6)
+expr = wt.contract(Fs2,minrank=0,maxrank=4)
 mbeq = expr.to_manybody_equations('fs2')
 for key in mbeq.keys():
 	for eq in mbeq[key]:
@@ -64,7 +65,7 @@ for key in mbeq.keys():
 # [W,s1_ext]
 print("[W,s1_ext]")
 Ws1 = w.commutator(W,s1_ext)
-expr = wt.contract(Ws1,minrank=0,maxrank=6)
+expr = wt.contract(Ws1,minrank=0,maxrank=4)
 mbeq = expr.to_manybody_equations('ws1')
 for key in mbeq.keys():
 	for eq in mbeq[key]:
@@ -73,7 +74,7 @@ for key in mbeq.keys():
 # [W,s2_ext]
 print("[W,s2_ext]")
 Ws2 = w.commutator(W,s2_ext)
-expr = wt.contract(Ws2,minrank=0,maxrank=6)
+expr = wt.contract(Ws2,minrank=0,maxrank=4)
 mbeq = expr.to_manybody_equations('ws2')
 for key in mbeq.keys():
 	for eq in mbeq[key]:
@@ -82,7 +83,7 @@ for key in mbeq.keys():
 # [[F,s1_ext],s1_ext]
 print("[[F,s1_ext],s1_ext]")
 Fs1s1 = w.commutator(F,s1_ext,s1_ext)
-expr = wt.contract(Fs1s1,minrank=0,maxrank=6)
+expr = wt.contract(Fs1s1,minrank=0,maxrank=4)
 mbeq = expr.to_manybody_equations('fs1s1')
 for key in mbeq.keys():
 	for eq in mbeq[key]:
@@ -91,7 +92,7 @@ for key in mbeq.keys():
 # [[F,s1_ext],s2_ext]
 print("[[F,s1_ext],s2_ext]")
 Fs1s2 = w.commutator(F,s1_ext,s2_ext)
-expr = wt.contract(Fs1s2,minrank=0,maxrank=6)
+expr = wt.contract(Fs1s2,minrank=0,maxrank=4)
 mbeq = expr.to_manybody_equations('fs1s2')
 for key in mbeq.keys():
 	for eq in mbeq[key]:
@@ -100,7 +101,7 @@ for key in mbeq.keys():
 # [[F,s2_ext],s1_ext]
 print("[[F,s2_ext],s1_ext]")
 Fs2s1 = w.commutator(F,s2_ext,s1_ext)
-expr = wt.contract(Fs2s1,minrank=0,maxrank=6)
+expr = wt.contract(Fs2s1,minrank=0,maxrank=4)
 mbeq = expr.to_manybody_equations('fs2s1')
 for key in mbeq.keys():
 	for eq in mbeq[key]:
@@ -109,7 +110,7 @@ for key in mbeq.keys():
 # [[F,s2_ext],s2_ext]
 print("[[F,s2_ext],s2_ext]")
 Fs2s2 = w.commutator(F,s2_ext,s2_ext)
-expr = wt.contract(Fs2s2,minrank=0,maxrank=6)
+expr = wt.contract(Fs2s2,minrank=0,maxrank=4)
 mbeq = expr.to_manybody_equations('fs2s2')
 for key in mbeq.keys():
 	for eq in mbeq[key]:
@@ -118,7 +119,7 @@ for key in mbeq.keys():
 # [[W,s1_ext],s1_ext]
 print("[[W,s1_ext],s1_ext]")
 Ws1s1 = w.commutator(W,s1_ext,s1_ext)
-expr = wt.contract(Ws1s1,minrank=0,maxrank=8)
+expr = wt.contract(Ws1s1,minrank=0,maxrank=4)
 mbeq = expr.to_manybody_equations('ws1s1')
 for key in mbeq.keys():
 	for eq in mbeq[key]:
@@ -127,7 +128,7 @@ for key in mbeq.keys():
 # [[W,s1_ext],s2_ext]
 print("[[W,s1_ext],s2_ext]")
 Ws1s2 = w.commutator(W,s1_ext,s2_ext)
-expr = wt.contract(Ws1s2,minrank=0,maxrank=8)
+expr = wt.contract(Ws1s2,minrank=0,maxrank=4)
 mbeq = expr.to_manybody_equations('ws1s2')
 for key in mbeq.keys():
 	for eq in mbeq[key]:
@@ -136,7 +137,7 @@ for key in mbeq.keys():
 # [[W,s2_ext],s1_ext]
 print("[[W,s2_ext],s1_ext]")
 Ws2s1 = w.commutator(W,s2_ext,s1_ext)
-expr = wt.contract(Ws2s1,minrank=0,maxrank=8)
+expr = wt.contract(Ws2s1,minrank=0,maxrank=4)
 mbeq = expr.to_manybody_equations('ws2s1')
 for key in mbeq.keys():
 	for eq in mbeq[key]:
@@ -145,7 +146,7 @@ for key in mbeq.keys():
 # [[W,s2_ext],s2_ext]
 print("[[W,s2_ext],s2_ext]")
 Ws2s2 = w.commutator(W,s2_ext,s2_ext)
-expr = wt.contract(Ws2s2,minrank=0,maxrank=8)
+expr = wt.contract(Ws2s2,minrank=0,maxrank=4)
 mbeq = expr.to_manybody_equations('ws2s2')
 for key in mbeq.keys():
 	for eq in mbeq[key]:
@@ -154,7 +155,7 @@ for key in mbeq.keys():
 # [[[F,s1_ext],s1_ext],s1_ext]
 print("[[[F,s1_ext],s1_ext],s1_ext]")
 Fs1s1s1 = w.commutator(F,s1_ext,s1_ext,s1_ext)
-expr = wt.contract(Fs1s1s1,minrank=0,maxrank=8)
+expr = wt.contract(Fs1s1s1,minrank=0,maxrank=4)
 mbeq = expr.to_manybody_equations('fs1s1s1')
 for key in mbeq.keys():
 	for eq in mbeq[key]:
@@ -163,7 +164,7 @@ for key in mbeq.keys():
 # [[[F,s1_ext],s1_ext],s2_ext]
 print("[[[F,s1_ext],s1_ext],s2_ext]")
 Fs1s1s2 = w.commutator(F,s1_ext,s1_ext,s2_ext)
-expr = wt.contract(Fs1s1s2,minrank=0,maxrank=8)
+expr = wt.contract(Fs1s1s2,minrank=0,maxrank=4)
 mbeq = expr.to_manybody_equations('fs1s1s2')
 for key in mbeq.keys():
 	for eq in mbeq[key]:
@@ -172,7 +173,7 @@ for key in mbeq.keys():
 # [[[F,s1_ext],s2_ext],s1_ext]
 print("[[[F,s1_ext],s2_ext],s1_ext]")
 Fs1s2s1 = w.commutator(F,s1_ext,s2_ext,s1_ext)
-expr = wt.contract(Fs1s2s1,minrank=0,maxrank=8)
+expr = wt.contract(Fs1s2s1,minrank=0,maxrank=4)
 mbeq = expr.to_manybody_equations('fs1s2s1')
 for key in mbeq.keys():
 	for eq in mbeq[key]:
@@ -181,7 +182,7 @@ for key in mbeq.keys():
 # [[[F,s1_ext],s2_ext],s2_ext]
 print("[[[F,s1_ext],s2_ext],s2_ext]")
 Fs1s2s2 = w.commutator(F,s1_ext,s2_ext,s2_ext)
-expr = wt.contract(Fs1s2s2,minrank=0,maxrank=8)
+expr = wt.contract(Fs1s2s2,minrank=0,maxrank=4)
 mbeq = expr.to_manybody_equations('fs1s2s2')
 for key in mbeq.keys():
 	for eq in mbeq[key]:
@@ -190,7 +191,7 @@ for key in mbeq.keys():
 # [[[F,s2_ext],s1_ext],s1_ext]
 print("[[[F,s2_ext],s1_ext],s1_ext]")
 Fs2s1s1 = w.commutator(F,s2_ext,s1_ext,s1_ext)
-expr = wt.contract(Fs2s1s1,minrank=0,maxrank=8)
+expr = wt.contract(Fs2s1s1,minrank=0,maxrank=4)
 mbeq = expr.to_manybody_equations('fs2s1s1')
 for key in mbeq.keys():
 	for eq in mbeq[key]:
@@ -199,7 +200,7 @@ for key in mbeq.keys():
 # [[[F,s2_ext],s1_ext],s2_ext]
 print("[[[F,s2_ext],s1_ext],s2_ext]")
 Fs2s1s2 = w.commutator(F,s2_ext,s1_ext,s2_ext)
-expr = wt.contract(Fs2s1s2,minrank=0,maxrank=8)
+expr = wt.contract(Fs2s1s2,minrank=0,maxrank=4)
 mbeq = expr.to_manybody_equations('fs2s1s2')
 for key in mbeq.keys():
 	for eq in mbeq[key]:
@@ -208,7 +209,7 @@ for key in mbeq.keys():
 # [[[F,s2_ext],s2_ext],s1_ext]
 print("[[[F,s2_ext],s2_ext],s1_ext]")
 Fs2s2s1 = w.commutator(F,s2_ext,s2_ext,s1_ext)
-expr = wt.contract(Fs2s2s1,minrank=0,maxrank=8)
+expr = wt.contract(Fs2s2s1,minrank=0,maxrank=4)
 mbeq = expr.to_manybody_equations('fs2s2s1')
 for key in mbeq.keys():
 	for eq in mbeq[key]:
@@ -217,7 +218,7 @@ for key in mbeq.keys():
 # [[[F,s2_ext],s2_ext],s2_ext]
 print("[[[F,s2_ext],s2_ext],s2_ext]")
 Fs2s2s2 = w.commutator(F,s2_ext,s2_ext,s2_ext)
-expr = wt.contract(Fs2s2s2,minrank=0,maxrank=8)
+expr = wt.contract(Fs2s2s2,minrank=0,maxrank=4)
 mbeq = expr.to_manybody_equations('fs2s2s2')
 for key in mbeq.keys():
 	for eq in mbeq[key]:
---
+++ downfolding/equation_gen/gen_ducc_eq_goal.py
@@ -0,0 +1,135 @@
+import wicked as w
+from itertools import product
+from IPython.display import display, Math, Latex
+
+def latex(expr):
+    """Render any object that has a latex() member."""
+    display(Math(expr.latex()))
+
+# ---------------------------------------------------------------------
+# Orbital spaces:
+#   O = external/core occupied
+#   o = active occupied
+#   v = active virtual
+#   V = external virtual
+# ---------------------------------------------------------------------
+w.reset_space()
+
+w.add_space("O", "fermion", "occupied",   ['I','J','K','L','M','N','P','Q'])
+w.add_space("o", "fermion", "occupied",   ['i','j','k','l','m','n','p','q'])
+w.add_space("v", "fermion", "unoccupied", ['a','b','c','d','e','g','x','y'])
+w.add_space("V", "fermion", "unoccupied", ['A','B','C','D','E','G','X','Y'])
+
+wt = w.WickTheorem()
+
+# All orbital spaces included in the Hamiltonian
+all_spaces = "OovV"
+
+# ---------------------------------------------------------------------
+# Fock and two-body interaction operators
+# ---------------------------------------------------------------------
+F = w.utils.gen_op('f', 1, all_spaces, all_spaces)
+W = w.utils.gen_op('v', 2, all_spaces, all_spaces)
+
+# ---------------------------------------------------------------------
+# External anti-Hermitian amplitudes
+#
+# Build all occupied -> virtual excitations:
+#     occupied:   O, o
+#     virtual:    v, V
+#
+# Then remove the purely active pieces:
+#     t1: v+ o
+#     t2: v+ v+ o o
+#
+# Therefore s_ext contains every amplitude touching at least one
+# external index O or V.
+# ---------------------------------------------------------------------
+
+# Singles:
+# Conceptually includes V+ o, v+ O, V+ O and adjoints.
+T1_all = w.utils.gen_op('t1', 1, 'vV', 'Oo', diagonal=False)
+T1_act = w.op('t1', ['v+ o'])
+T1_ext = T1_all - T1_act
+s1_ext = T1_ext - T1_ext.adjoint()
+
+# Doubles:
+# Conceptually includes all {v,V}{v,V} <- {O,o}{O,o}
+# blocks except the purely active v v <- o o block.
+T2_all = w.utils.gen_op('t2', 2, 'vV', 'Oo', diagonal=False)
+T2_act = w.op('t2', ['v+ v+ o o'])
+T2_ext = T2_all - T2_act
+s2_ext = T2_ext - T2_ext.adjoint()
+
+# Optional checks
+# print("s1_ext")
+# print(s1_ext)
+# print("s2_ext")
+# print(s2_ext)
+
+# ---------------------------------------------------------------------
+# Helpers for printing many-body equations
+# ---------------------------------------------------------------------
+S = {
+    "s1": s1_ext,
+    "s2": s2_ext,
+}
+
+def pretty_nested_commutator(base, seq):
+    """
+    Example:
+        base='F', seq=('s1','s2') -> [[F,s1_ext],s2_ext]
+    """
+    text = base
+    for name in seq:
+        text = f"[{text},{name}_ext]"
+    return text
+
+def print_manybody_equations(label, op, minrank=0, maxrank=8):
+    expr = wt.contract(op, minrank=minrank, maxrank=maxrank)
+    mbeq = expr.to_manybody_equations(label)
+
+    for key in mbeq.keys():
+        for eq in mbeq[key]:
+            print(eq.compile('einsum'))
+
+def run_commutator(h_code, h_print, H, seq, maxrank):
+    """
+    h_code  : label prefix used for tensor/equation names, e.g. 'f' or 'w'
+    h_print : printed operator name, e.g. 'F' or 'W'
+    H       : Wicked operator
+    seq     : tuple such as ('s1', 's2')
+    maxrank : max rank for Wick contraction
+    """
+    label = h_code + ''.join(seq)
+    print(pretty_nested_commutator(h_print, seq))
+
+    comm = w.commutator(H, *[S[name] for name in seq])
+    print_manybody_equations(label, comm, minrank=0, maxrank=maxrank)
+
+
+
+
+# First order:
+#   [F,s1], [F,s2], [W,s1], [W,s2]
+for h_code, h_print, H in [
+    ('f', 'F', F),
+    ('w', 'W', W),
+]:
+    for seq in [('s1',), ('s2',)]:
+        run_commutator(h_code, h_print, H, seq, maxrank=4)
+
+# Second order:
+#   [[F,si],sj]
+#   [[W,si],sj]
+for h_code, h_print, H, maxrank in [
+    ('f', 'F', F, 4),
+    ('w', 'W', W, 4),
+]:
+    for seq in product(('s1', 's2'), repeat=2):
+        run_commutator(h_code, h_print, H, seq, maxrank=maxrank)
+
+# Third order with F:
+#   [[[F,si],sj],sk]
+for seq in product(('s1', 's2'), repeat=3):
+    run_commutator('f', 'F', F, seq, maxrank=4)
